47f176d5dcf96cfdb58c23691dbc5dd9e985f04a
joeboe22 <74623495+joeboe22@users.noreply.github.com>
2021-02-15 18:45:50 -0600

    Update 411.sql

--- 411.sql
+++ 411.sql
@@ -73,7 +73,7 @@ VALUES(10234,'342 streetA','Monticello','MN');
 INSERT INTO Address(pid,streetAddress,city,state) 
 VALUES(11567,'344 streetA','Monticello','MN');
 INSERT INTO Address(pid,streetAddress,city,state) 
-VALUES( ,'65 streetS','StCloud','MN');
+VALUES(NULL ,'65 streetS','StCloud','MN');
 INSERT INTO Address(pid,streetAddress,city,state) 
 VALUES(3289,'892 streetM','Minneapolis','MN');
 INSERT INTO Address(pid,streetAddress,city,state) 
@@ -83,9 +83,41 @@ VALUES(88331,'342 streetC','Monticello','MN');
 INSERT INTO Address(pid,streetAddress,city,state) 
 VALUES(2557,'6 streetA','StPaul','MN');
 INSERT INTO Address(pid,streetAddress,city,state) 
-VALUES( ,'789 streetE','Duluth','MN');
+VALUES(NULL ,'789 streetE','Duluth','MN');
 INSERT INTO Address(pid,streetAddress,city,state) 
 VALUES(74591,'999 streetG','Duluth','MN');
+-----------------------------------------
+--------------------------------
+create table Forecast(
+fid char(10),
+city char(20),
+high integer,
+low integer,
+forecast_date date,
+primary key(fid)
+
+--pid integer,  -- For FKs to link relationships called: views and presents
+--oid integer, -- for FKs to link relationship called: makes
+
+-- foreign key (pid) references Consumers (pid), -- Views
+-- foreign key (pid) references Meteorologists (pid), -- Presents
+-- foreign key (oid) references Observation (oid) -- Makes
+);
+--------------------------
+INSERT INTO Forecast(fid, high, low, city, forecast_date)
+VALUES('F02',30,17,'St. Cloud', TO_DATE('31-DEC-2020', 'DD-MON-YYYY'));
+INSERT INTO Forecast(fid, high, low, city, forecast_date)
+VALUES('F03',20,7,'Duluth', TO_DATE('31-DEC-2020', 'DD-MON-YYYY'));
+INSERT INTO Forecast(fid, high, low, city, forecast_date)
+VALUES('F04',37,30,'Minneapolis', TO_DATE('31-DEC-2020', 'DD-MON-YYYY'));
+INSERT INTO Forecast(fid, high, low, city, forecast_date)
+VALUES('F05',42,31,'St. Cloud', TO_DATE('30-NOV-2020', 'DD-MON-YYYY'));
+INSERT INTO Forecast(fid, high, low, city, forecast_date)
+VALUES('F08',81,56,'Duluth', TO_DATE('15-JAN-2021', 'DD-MON-YYYY'));
+INSERT INTO Forecast(fid, high, low, city, forecast_date)
+VALUES('F09',76,62,'Minneapolis', TO_DATE('15-JAN-2021', 'DD-MON-YYYY'));
+INSERT INTO Forecast(fid, high, low, city, forecast_date)
+VALUES('F15',95,72,'St.Cloud', TO_DATE('03-FEB-2021', 'DD-MON-YYYY'));
 -----------------------------------------
 --------------------------------
 
