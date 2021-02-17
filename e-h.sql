89812a5ea59f7141d4d7732289e8cf0604456718
joeboe22 <74623495+joeboe22@users.noreply.github.com>
2021-02-17 17:30:39 -0600

    Update e-h.sql

--- e-h.sql
+++ e-h.sql
@@ -16,3 +16,13 @@ AND address.pid NOT IN
     from observers);
 /* output: 6 street A, St.Paul, MN */   
 /* correct because that is bill's address and he is an outsider */
+
+/* PROBLEM F */
+
+/* PROBLEM G */
+SELECT observations.city, observations.ob_date, observations.temperature, forecast.low 
+FROM observations, forecast
+WHERE forecast.city = observations.city
+AND forecast.forecast_date = observations.ob_date
+AND forecast.low > observations.temperature /* FORECASTED LOW was higher than the actual temp */
+ORDER BY observations.ob_date, observations.city;
