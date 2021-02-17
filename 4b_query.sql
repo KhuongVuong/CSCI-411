cb784b0fe3a1a3a44f3c752371644127fc907199
joeboe22 <74623495+joeboe22@users.noreply.github.com>
2021-02-17 16:57:55 -0600

    Update and rename 4b_query.sql to drop_ALLsql

--- 4b_query.sql
+++
@@ -1,3 +0,0 @@
-select DISTINCT person.name, consumers.pid from consumers, person ,viewed, observations, address 
-where consumers.pid = viewed.pid AND viewed.pid = person.pid AND observations.pid = address.pid AND observations.city = address.city
-ORDER BY person.name, consumers.pid
