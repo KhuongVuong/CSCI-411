53cf82702dd75780b295ffb841839953a4237c3e
Jay1625 <73314252+Jay1625@users.noreply.github.com>
2021-02-19 08:53:15 -0600

    Update emm.sql

--- emm.sql
+++ emm.sql
@@ -11,7 +11,7 @@ PID	Numcount
 	3289	4
 	10234	0
 	11567	6
-88331	4
+	88331	4
 
 JUSTIFICATION:  the person id and number of observations is in the output
 
@@ -27,15 +27,15 @@ WHERE person.pid = address.pid
 GROUP BY person.pid person.name;
 ORDER BY person.pid
 /*OUTPUT
-PID	NAME		CITY		Numcount
+	PID	NAME		CITY		Numcount
 ------------------------------------------------------------------------------
-	2577	Bill		St. Paul		1
-3289	George		Minneapolis	1
-74591	Jane		Duluth		1
+	2577	Bill		St. Paul	1
+	3289	George		Minneapolis	1
+	74591	Jane		Duluth		1
 	10234	Able		Monticello	4
 	11567	Baker		Monticello	4
-17645	Mary		Monticello	4
-88331	Alice		Monticello	4
+	17645	Mary		Monticello	4
+	88331	Alice		Monticello	4
 
 JUSTIFICATION:  the person id and name, the city name and number of person in list who is living in the city are in the output
 
